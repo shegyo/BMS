e734e4ff125bff9b29b079408f3daaf14c1698ac
shegyo <khalidfinisher@web.de>
2024-03-29 21:29:31 +0100

    asdfas

--- cogs/Utility.py
+++ cogs/Utility.py
@@ -1,4 +1,4 @@
-from discord.ext import commands, tasks
+from discord.ext import commands
 import discord
 from discord import app_commands
 
@@ -22,7 +22,6 @@ class Utility(commands.Cog):
   
   def __init__(self, bot):
     self.bot = bot
-    self.create_channels.start()
     
   # Help Command -> get Support
   @app_commands.command(description="bot help")
@@ -30,9 +29,8 @@ class Utility(commands.Cog):
     description = "For help and Support, please contact the System Administrators!\n\n"
     description += "<:discord:1216307276927733800> <@324607583841419276>\n"
     description += "<:discord:1216307276927733800> <@818879706350092298>\n"
-    description += "📧 brawltourneysystem@gmail.com"
     embed = discord.Embed(title="SUPPORT", description=description, color=int("000000", 16))
-    embed.set_image(url="https://media.discordapp.net/attachments/1216040586348593213/1217918220539789382/BTSystem_Logo.jpg")
+    embed.set_image(url="https://media.discordapp.net/attachments/1216040586348593213/1223366470088790046/bms_avatar.jpg")
     viewItems = [LinkButton("Linktree", "https://linktr.ee/bsystems")]
     await interaction.response.send_message(embed=embed, view=View(viewItems))
 
@@ -44,23 +42,6 @@ class Utility(commands.Cog):
     embed = discord.Embed(title="<:discord:1216307276927733800>  Invite me to your Server!", description=description, color=int("000000", 16))
     viewItems = [LinkButton("Invite me!", "https://discord.com/oauth2/authorize?client_id=1223344546260193280&permissions=8&scope=bot")]
     await interaction.response.send_message(embed=embed, view=View(viewItems))
-  
-
-  #loop for creating channels
-  @tasks.loop(minutes=1)
-  async def create_channels(self):
-    for guild in self.bot.guilds:
-      # Kategorie erstmal finden
-      findMatesCategory = discord.utils.get(guild.categories, name="FIND MATES")
-      # Erstellen falls nicht da
-      if not findMatesCategory:
-        findMatesCategory = await guild.create_category_channel("FIND MATES")
-        # Kanäle erstellen
-        onlyRead = {
-          guild.default_role: discord.PermissionOverwrite(send_messages=False),
-        }
-        await findMatesCategory.create_text_channel("find-mates", overwrites=onlyRead, topic="find a team to join in this channel")
-        await findMatesCategory.create_text_channel("search-mates", topic="run /find_mates to post your search!")
 
 
 async def setup(bot):
--- cogs/findTeams.py
+++ cogs/findTeams.py
@@ -1,27 +1,57 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 import discord, requests
 from discord import app_commands
 from cogs.Utility import View
 
 gamemodes = requests.get("https://api.brawlapi.com/v1/gamemodes").json()["list"]
-print(gamemodes)
+
+
+# Formular to fill out, creates the Search post
+class TicketModal(discord.ui.Modal):
+  def __init__(self, bot):
+    super().__init__(title="Create Ticket")
+    self.bot = bot
+
+  topic = discord.ui.TextInput(label="Topic", style=discord.TextStyle.short, required=True, min_length=3, max_length=30)
+  description = discord.ui.TextInput(label="Description", style=discord.TextStyle.long, min_length=10, max_length=200)
+
+  async def on_submit(self, interaction: discord.Interaction):
+    pass
 
 # The Commands
 class findTeams(commands.Cog):
 
   def __init__(self, bot):
     self.bot = bot
+    self.create_channels.start()
 
   @app_commands.command(description="Creates a Support Ticket")
   @app_commands.checks.cooldown(1, 60*5, key=lambda i: (i.guild_id, i.user.id))
   async def find_mates(self, interaction: discord.Interaction, your_total_trophies: int, trophy_minimum: int, game_mode: str, team_code: str):
-    
-    await interaction.response.send_message("Search posted on all servers!")
+    await interaction.response.send_modal(TicketModal(self.bot))
 
   @find_mates.error
   async def ticket_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
     if isinstance(error, app_commands.CommandOnCooldown):
         await interaction.response.send_message(str(error), ephemeral=True)
+
+  
+
+  #loop for creating channels
+  @tasks.loop(minutes=1)
+  async def create_channels(self):
+    for guild in self.bot.guilds:
+      # Kategorie erstmal finden
+      findMatesCategory = discord.utils.get(guild.categories, name="FIND MATES")
+      # Erstellen falls nicht da
+      if not findMatesCategory:
+        findMatesCategory = await guild.create_category_channel("FIND MATES")
+        # Kanäle erstellen
+        onlyRead = {
+          guild.default_role: discord.PermissionOverwrite(send_messages=False),
+        }
+        await findMatesCategory.create_text_channel("find-mates", overwrites=onlyRead, topic="find a team to join in this channel")
+        await findMatesCategory.create_text_channel("search-mates", topic="run /find_mates to post your search!")
 
 
 async def setup(bot):
