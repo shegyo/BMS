1331e5c1f13b12cb9b7787d7d019bdde1589cfe7
shegyo <khalidfinisher@web.de>
2024-05-02 17:36:20 +0200

    asfasdfa

--- cogs/bsChallenges.py
+++ cogs/bsChallenges.py
@@ -13,27 +13,6 @@ class bsChallenges(commands.Cog):
   
   def __init__(self, bot):
     self.bot = bot
-    
-  # Get your Random Challenge
-  @app_commands.command(description="get a random bs challenge (soon)")
-  async def challenge(self, interaction: discord.Interaction, challenge: str):
-    # Fetch selected language
-    options = mongodb.findGuildOptions(interaction.guild.id)
-    language = options["language"]
-
-    challenge = random.choice(bsChallengesList)
-    description = challenge["content"][language]
-    embed = discord.Embed(title=challenge["title"][language], description=description, color=int("000000", 16))
-    
-    await interaction.response.send_message(content="", attachments=[discord.File("playerNotFound.webp", filename="playerNotFound.webp")], embed=embed)
-
-      
-  # @challenge.error
-  # async def challenge_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
-  #   if isinstance(error, app_commands.NoPrivateMessage):
-  #     return await interaction.response.send_message("Command can't be run in Private Messages.", delete_after=5, ephemeral=True)
-  #   else:
-  #     return await interaction.response.send_message(f"Unknown error: {error}", delete_after=5, ephemeral=True)
 
   # Get your Random Challenge
   @app_commands.command(description="get a random bs challenge")
